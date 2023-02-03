8ec7d912aea9d8a786a60de289e783cb825bb8d6
Jose Vaquero Padilla <jose.vaquero2003@gmail.com>
2023-02-03 13:38:48 +0100

    project complete

---
+++ src/main/java/com/example/Spring/boot/starting/SpringBootStartingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.boot.starting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStartingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring/boot/starting/SpringBootStartingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.boot.starting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStartingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
